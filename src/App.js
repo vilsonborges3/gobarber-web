5f3a39cc9a970a67352c3fc91224bacb19c6fd41
vilson borges <vilsonborges98@gmail.com>
2020-08-08 16:00:47 -0300

    return more date need in the web version

--- src/App.js
+++ src/App.js
@@ -2,9 +2,7 @@ import React from 'react';
 
 function App() {
   return (
-    <div className="App">
-      <h1>Hello world</h1>
-    </div>
+    <div className="App"><h1>Hello world</h1></div>
   );
 }
 
