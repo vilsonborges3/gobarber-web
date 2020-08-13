dc596cd8c5450e686612ded7b5022b8bb905f0c4
vilson borges <vilsonborges98@gmail.com>
2020-08-13 18:23:52 -0300

    ending the notification and update profile and add avatar

--- src/pages/Profile/index.js
+++ src/pages/Profile/index.js
@@ -1,9 +1,45 @@
 import React from 'react';
+import { Form, Input } from '@rocketseat/unform';
+import { useSelector, useDispatch } from 'react-redux';
 
-// import { Container } from './styles';
+import { updateProfileRequest } from '~/store/modules/user/actions';
 
-function Profile() {
-  return <div />;
+import { Container } from './styles';
+import AvatarInput from './AvatarInput';
+
+export default function Profile() {
+  const dispatch = useDispatch();
+
+  const profile = useSelector((state) => state.user.profile);
+
+  function handleSubmit(data) {
+    dispatch(updateProfileRequest(data));
+  }
+  return (
+    <Container>
+      <Form initialData={profile} onSubmit={handleSubmit}>
+        <AvatarInput name="avatar_id" />
+        <Input name="name" placeholder="Nome completo" />
+        <Input name="email" type="email" placeholder="Seu email" />
+
+        <hr />
+
+        <Input
+          type="password"
+          name="oldPassword"
+          placeholder="Sua senha atual"
+        />
+        <Input type="password" name="password" placeholder="Nova senha" />
+        <Input
+          type="password"
+          name="confirmPassword"
+          placeholder="Confirmação de senha"
+        />
+
+        <button type="submit">Atualizar perfil</button>
+      </Form>
+
+      <button type="button">Sair do GoBarber</button>
+    </Container>
+  );
 }
-
-export default Profile;
